fd40ac038886c634f1fa03b13f84c22693fe3a62
Jordon Bedwell <jordon@envygeeks.io>
2017-10-12 06:57:48 -0500

    Do some cleanup.

--- lib/liquid/tag/parser.rb
+++ lib/liquid/tag/parser.rb
@@ -26,18 +26,17 @@ module Liquid
       rb_delegate :args_with_indifferent_access, to: :@args, \
         alias_of: :with_indifferent_access
 
-      FLOAT_REGEXP = /^\d+\.\d+$/
-      BOOLEAN_REGEXP = /^(?<!\\)(\!|@)/
-      NEGATIVE_BOOLEAN_REGEXP = /^(?<!\\)\!/
-      BOOLEAN_QUOTE_REGEXP = /^('|")((?<!\\)@|(?<!\\)\!)/
-      SHELLWORDS_REGEXP = /\G\s*(?>([^\s\\\'\"]+)|'([^\']*)'|"((?:[^\"\\]|\\.)*)"|(\\.?)|(\S))(\s|\z)?/m
+      FLOAT = /^\d+\.\d+$/
+      BOOLEAN = /^(?<!\\)(\!|@)/
+      NEGATIVE_BOOLEAN = /^(?<!\\)\!/
       BOOLEAN_QUOTE_REPLACEMENT = "\\1\\\\\\2"
-      POSITIVE_BOOLEAN_REGEXP = /^(?<!\\)\@/
-      DESCAPED_BOOLEAN_REGEXP = /\\(@|\!)/
-      KEY_REGEXP = /\b(?<!\\):/
-      INT_REGEXP = /^\d+$/
+      SHELLSPLIT = /\G\s*(?>([^\s\\\'\"]+)|'([^\']*)'|"((?:[^\"\\]|\\.)*)"|(\\.?)|(\S))(\s|\z)?/m
+      BOOLEAN_QUOTE = /^('|")((?<!\\)@|(?<!\\)\!)/
+      POSITIVE_BOOLEAN = /^(?<!\\)\@/
+      ESCAPED_BOOLEAN = /\\(@|\!)/
+      KEY = /\b(?<!\\):/
+      INT = /^\d+$/
 
-      # --
       def initialize(raw, defaults: {}, sep: "=")
         @sep = sep
         @rsep = Regexp.escape(sep)
@@ -51,12 +50,13 @@ module Liquid
         parse
       end
 
-      # --
       def to_html(skip: [], hash: false)
         out = @args.each_with_object(hash ? {} : []) do |(k, v), o|
-          next if k == :argv1 || skip.include?(k) ||
-            v == false || v.is_a?(Hash) ||
-            v.is_a?(Array)
+          next if k == :argv1
+          next if v.is_a?(Array)
+          next if skip.include?(k)
+          next if v.is_a?(Hash)
+          next if v == false
 
           o[k] = v if hash
           unless hash
@@ -67,29 +67,32 @@ module Liquid
         hash ? out : out.join(" ")
       end
 
-      # --
       private
-      def parse
-        args, hash = from_shellwords, {}
-        if args.first !~ BOOLEAN_REGEXP && args.first !~ KEY_REGEXP \
-        && args.first !~ @sep_regexp
+      def argv1(args)
+        val = args[0]
+        hash = {}
 
+        # !"@true" && !"key1:key2=val" but "argv1 @true key1:key2=val"
+        if val !~ BOOLEAN && val !~ KEY && val !~ @sep_regexp
           hash = {
-            :argv1 => args.delete_at(0)
+            argv1: args.delete_at(0)
           }
         end
 
+        return args, hash
+      end
+
+      private
+      def parse
+        args, hash = argv1(from_shellwords)
         @args = @defaults.deep_merge(args.each_with_object(hash) do |k, h, out = h|
           keys, _, val = k.rpartition(@sep_regexp)
 
-          val  = val.gsub(@escaped_sep_regexp, @sep)
-          if keys.empty? && val =~ BOOLEAN_REGEXP
-            # @true, @false will not map right.
-            keys = val.gsub(BOOLEAN_REGEXP, "")
-          end
+          val  = val.gsub(@escaped_sep_regexp, @sep) # Unescape \\=
+          keys = val.gsub(BOOLEAN, "") if keys.empty? && val =~ BOOLEAN # @true
+          keys,  val = val, nil if keys.empty? # key val
+          keys = keys.split(KEY).map(&:to_sym)
 
-          keys, val = val, nil if keys.empty?
-          keys = keys.split(KEY_REGEXP).map(&:to_sym)
           if keys.size > 1
             h = h[keys[0]] ||= {}
             keys[1...-1].each do |sk|
@@ -97,16 +100,15 @@ module Liquid
             end
           end
 
-
-          val = false if val == "false"
-          val = val.to_f if val =~ FLOAT_REGEXP
-          val = false if val =~ NEGATIVE_BOOLEAN_REGEXP
-          val = true  if val =~ POSITIVE_BOOLEAN_REGEXP
-          val = val.to_i if val =~ INT_REGEXP
-          val = true if val == "true"
+          val = val.to_i if val =~ INT # key=1
+          val = val.to_f if val =~ FLOAT # key=0.1
+          val = false    if val == "false" # key=false
+          val = false    if val =~ NEGATIVE_BOOLEAN # !false
+          val = true     if val =~ POSITIVE_BOOLEAN # @true
+          val = true     if val == "true" # key=true
 
           if val.is_a?(String)
-            then val = val.gsub(DESCAPED_BOOLEAN_REGEXP, "\\1")
+            then val = val.gsub(ESCAPED_BOOLEAN, "\\1")
           end
 
           key = keys.last.to_sym
@@ -121,8 +123,7 @@ module Liquid
       private
       def from_shellwords
         shellsplit(@raw.gsub(/('|")([^\1]+)\1/) do |v|
-          v.gsub(BOOLEAN_QUOTE_REGEXP, BOOLEAN_QUOTE_REPLACEMENT).
-            gsub(@sep_regexp, @escaped_sep)
+          v.gsub(BOOLEAN_QUOTE, BOOLEAN_QUOTE_REPLACEMENT).gsub(@sep_regexp, @escaped_sep)
         end)
       end
 
@@ -134,10 +135,11 @@ module Liquid
       def shellsplit(line)
         out, field = [], ""
 
-        line.scan(SHELLWORDS_REGEXP) do |w, s, d, e, g, se|
+        line.scan(SHELLSPLIT) do |w, s, d, e, g, se|
           raise ArgumentError, "Unmatched double quote: #{line.inspect}" if g
           field = field + (w || s || (d && d.gsub(/\\([$`"\\\n])/, '\\1')) \
-          || e.gsub(/\\(.)/, '\\1'))
+            || e.gsub(/\\(.)/, '\\1'))
+
           if se
             out << field
             field = ""
